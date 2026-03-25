b2caee7225ba72bb4fa357d18f1b494d21d7691c
Mihnea-Rares <ciocoiu.mihnea.d3y@student.ucv.ro>
2026-03-25 21:19:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+
+
+namespace ProiectAPD
+{
+    
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+            string filePath = "date.txt";
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Fișierul nu există!");
+                return;
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            foreach (string line in File.ReadLines(filePath))
+            {
+                string[] words = line.Split(new[] { ' ', '.', ',', '!' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string word in words)
+                {
+                    string cleanWord = word.ToLower();
+                    if (wordCounts.ContainsKey(cleanWord))
+                        wordCounts[cleanWord]++;
+                    else
+                        wordCounts[cleanWord] = 1;
+                }
+            }
+
+            stopwatch.Stop(); 
+            Console.WriteLine($"Gata! Timp: {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
+
+
